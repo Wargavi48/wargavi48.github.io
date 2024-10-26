025e703d7d1ff01655679a9601d98a2e40b1be6c
Mohamad Alief Rizky Ramadhan <alief.ramadhan@pasarpolis.com>
2024-10-26 13:18:18 +0700

    feat: add Comic constant file

---
+++ src/Constant/Comic.ts
@@ -0,0 +1,19 @@
+const comicData = [
+    { id: 1, thumbnail: '/Comic/Thumbnail/Lemon.jpg', title: 'Lemon' },
+    { id: 2, thumbnail: '/Comic/Thumbnail/Tini Nyangkut.png', title: 'Tini Nyangkut' },
+]
+
+export const comicContent = {
+    "Lemon": [
+        '/Comic/Lemon/1.jpeg'
+    ],
+    "Tini Nyangkut": [
+        '/Comic/Tini Nyangkut/komik_ktp_1.jpg',
+        '/Comic/Tini Nyangkut/komik_ktp_2.jpg',
+        '/Comic/Tini Nyangkut/komik_ktp_3.jpg',
+        '/Comic/Tini Nyangkut/komik_ktp_4.jpg',
+        '/Comic/Tini Nyangkut/komik_ktp_5.jpg',
+    ]
+}
+
+export default comicData
