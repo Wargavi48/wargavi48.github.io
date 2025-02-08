d15e8acbe3a3a282dac8e94440407ef1ecbee2b9
schkj <sonimiftahpratama@gmail.com>
2025-02-08 18:03:24 +0700

    fix: fix shopping icon

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import projectData from './Constant/Project'
 import comicData from './Constant/Comic'
 import ProjectCardSlider from './components/ProjectCardSlider'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDiscord, faXTwitter, faCartShopping, faSquareSteam  } from '@fortawesome/free-brands-svg-icons';
+import { faDiscord, faXTwitter, faSquareSteam  } from '@fortawesome/free-brands-svg-icons';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 
 function App() {
@@ -40,7 +41,7 @@ function App() {
                     <FontAwesomeIcon icon={faSquareSteam} size="lg" className="text-[#1b2738]" />
                   </a>
                   <a href="https://www.tokopedia.com/wargavi48" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faCartShopping} size="lg" className="text-[#15b11f]" />
+                    <FontAwesomeIcon icon={faShoppingCart} size="lg" className="text-[#15b11f]" />
                   </a>
                   <a href="https://trakteer.id/wargavi48" target="_blank"><img id="wse-buttons-preview" src="https://edge-cdn.trakteer.id/images/embed/trbtn-red-1.png?v=24-01-2025" height="40" style="border:0px;height:40px;" alt="Trakteer Saya"></a>
                 </div>
