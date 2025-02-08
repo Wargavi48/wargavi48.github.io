8569ba329bd9cc387897d2920d5e0ba66234bee7
schkj <sonimiftahpratama@gmail.com>
2025-02-08 14:56:06 +0700

    feat: add 'haircut' comic

--- src/Constant/Comic.ts
+++ src/Constant/Comic.ts
@@ -1,6 +1,7 @@
 const comicData = [
     { id: 1, thumbnail: '/Comic/Thumbnail/Lemon.jpg', title: 'Lemon' },
     { id: 2, thumbnail: '/Comic/Thumbnail/Tini Nyangkut.png', title: 'Tini Nyangkut' },
+    { id: 3, thumbnail: '/Comic/Thumbnail/Haircut.png', title: 'a little bit more... ✂️' },
 ]
 
 export const comicContent = {
@@ -13,6 +14,9 @@ export const comicContent = {
         '/Comic/Tini Nyangkut/komik_ktp_3.jpg',
         '/Comic/Tini Nyangkut/komik_ktp_4.jpg',
         '/Comic/Tini Nyangkut/komik_ktp_5.jpg',
+    ],
+    "a little bit more... ✂️": [
+        '/Comic/Haircut/Haircut.jpg'
     ]
 }
 
